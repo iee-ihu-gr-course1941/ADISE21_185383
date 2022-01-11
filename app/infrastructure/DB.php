045d478fdbc88872b9bba17b117b9ce777c5613a
george kalleas <georgekalleas@outlook.com>
2022-01-11 21:21:18 +0200

    api: mysqli

--- app/infrastructure/DB.php
+++ app/infrastructure/DB.php
@@ -3,18 +3,11 @@ class DB {
     private static $user = 'root';
     private static $password = 'root';
     private static $database = 'adise21_185383';
-	// locally
-	private static $host = 'localhost';
-	private static $sock = ''; 
-	// users.it.teithe.gr
-	//private static $host = '';
-    //private static $sock = '/home/student/it/2018/it185383/mysql/run/mysql.sock'; 
+	private static $host = '';
+    private static $sock = '/home/student/it/2018/it185383/mysql/run/mysql.sock'; 
 
     public static function getConnection(){		
-		// locally
-		$conn = new mysqli(self::$host, self::$user, self::$password, self::$database);
-		// users.it.teithe.gr
-		//$conn = new mysqli(self::$host, self::$user, self::$password, self::$database, null, self::$sock);
+		$conn = new mysqli(self::$host, self::$user, self::$password, self::$database, null, self::$sock);
 
 		if($conn->connect_error){
 			die("Error failed to connect to MySQL: " . $conn->connect_error);
@@ -23,4 +16,3 @@ class DB {
 		}
     }
 }
-?>
